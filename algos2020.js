0278ac1b3c90506aa0ba6d449556ee493f1c96e3
Michael Cote <michaelcote7@outlook.com>
2020-08-06 20:52:51 -0700

    .

---
+++ algos2020.js
@@ -0,0 +1,34 @@
+
+// let nums = [2,7,11,15];
+// let target = 26;
+
+// let nums = [3,3];
+// let target = 6;
+
+// var twoSum = function(nums, target) {
+//   for(var i = 0; i<nums.length ; i++){
+//       for(var j = i+1; j<nums.length ; j++){
+//           if(nums[i] + nums[j] === target){
+//               return [i,j]
+//           }
+//       }
+//   }
+// };
+// console.log(twoSum(nums, target));
+
+// const isPalindrome = (x) => {
+//   x = x + "";
+//   let n = x.split("").reverse().join("");
+//   return x === n ? true : false;
+// };
+// console.log(isPalindrome(12156));
+
+// [3,2,2,3]
+// 3
+// return length = 2
+// var removeElement = function(nums, val) {
+//   let result = nums.filter(n => n !== val);
+//   nums = result.length;
+//   return nums;
+// };
+// console.log(removeElement([3,2,2,3], 3));
