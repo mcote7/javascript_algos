60bd0bf7c5d43276f4cb8d2b02eb9558ad0d3067
Michael Cote <michaelcote7@outlook.com>
2020-09-23 12:40:06 -0700

    .

--- 9_22_2020.js
+++ 9_22_2020.js
@@ -8,3 +8,16 @@
 // }
 // console.log(duplicateNums(arr1, arr2));
 
+// **********************************************
+
+// Given an array, shift all values one position to the left.  Change the final position to 0. Return the changed array
+// Predicted Output: shiftLeft([1,2,3,4,5]) should return [2,3,4,5,0];
+
+// const shiftLeft = (arr) => {
+//   let result = [...arr];
+//   result.shift()
+//   // console.log("xx",result);
+//   result.push(0);
+//   return result;
+// };
+// console.log(shiftLeft([1,2,3,4,5]));
