f955328bb6a7bf31ad50960d9624710767180dc0
Michael Cote <michaelcote7@outlook.com>
2021-04-12 09:03:25 -0700

    .

--- again.js
+++ again.js
@@ -1,1 +1,1 @@
-console.log(Math.random().toString(36).slice(2))
+console.log(Math.random().toString(36).slice(2));
---
+++ new.js
@@ -0,0 +1,21 @@
+// helper function for ngIf's 
+
+// const and = (...x) => x.reduce( (x,y) => x && y );
+
+// let boola = true;
+// let boolb = false;
+// let boolc = true;
+// let boold = false;
+
+// console.log(and(boola, !boolb, boolc, !boold));
+
+// ex. 
+
+// ngIf*="boola && !boolb && boolc && !boold" 
+
+// ngIf*="and(boola, !boolb, boolc, !boold)" 
+
+
+// let array = [1,2,3,4,5];
+
+// console.log(array[4]);
