41657a75f3218d912414bfa0952af51e1785a8dc
Michael Cote <michaelcote7@outlook.com>
2021-07-07 10:09:36 -0700

    .

--- new.js
+++ new.js
@@ -19,3 +19,8 @@
 // let array = [1,2,3,4,5];
 
 // console.log(array[4]);
+
+// merge two sorted arrays
+const merge = (arr1, arr2) => {if (arr1.length && arr2.length) return result = arr1.concat(arr2).sort((a,b)=> {return a - b}); else return 'no length';};
+console.log("merge success? :::",merge([4,7,2,9], [3,6,5,8]));
+console.log("merge success? :::",merge([], []));
