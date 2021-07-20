a2f1f04b627dbfc29d8593edc813b812aa1d5b8a
Michael Cote <michaelcote7@outlook.com>
2021-07-19 18:01:13 -0700

    .

--- again.js
+++ again.js
@@ -1,1 +1,11 @@
-console.log(Math.random().toString(36).slice(2));
+// console.log(Math.random().toString(36).slice(2));
+
+let string = "25";
+
+let number = +string;
+
+let newRes = parseInt(string, 10);
+
+console.log('string', string);
+console.log('number', number);
+console.log('number new', newRes);
